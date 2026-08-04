88c3271fbc0232080f413572130485084f951ffa
liaojiajunj1325-ux <liaojiajunj1325@gmail.com>
2026-08-04 19:53:15 +0800

    换个文件测试

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,4 @@
-export function initNav() {
+(function () {
   var path = location.pathname.split("/").pop() || "index.html";
   var links = document.querySelectorAll(".nav-link");
   for (var i = 0; i < links.length; i++) {
@@ -6,4 +6,4 @@ export function initNav() {
     if (href === path) links[i].classList.add("active");
     else links[i].classList.remove("active");
   }
-}
+})();
